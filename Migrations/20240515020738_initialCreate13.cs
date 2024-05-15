a1f82b608a50c6f66cab8857abee1e26e81435a9
Vondreii <Sharlene.VonDrehnen@outlook.com>
2024-05-15 12:32:14 +1000

    Updated images for countries in DB

---
+++ Migrations/20240515020738_initialCreate13.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelOrganiserTool.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageFilename",
+                value: "belgium.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageFilename",
+                value: "denmark.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "ImageFilename",
+                value: "finland.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "ImageFilename",
+                value: "france.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "ImageFilename",
+                value: "germany.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "ImageFilename",
+                value: "ireland.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImageFilename",
+                value: "italy.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImageFilename",
+                value: "norway.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "ImageFilename",
+                value: "united-kingdom.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "ImageFilename",
+                value: "bahamas.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "ImageFilename",
+                value: "canada.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "ImageFilename",
+                value: "jamaica.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "ImageFilename",
+                value: "mexico.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "ImageFilename",
+                value: "united-states.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "ImageFilename",
+                value: "argentina.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "ImageFilename",
+                value: "brazil.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "ImageFilename",
+                value: "chile.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 28,
+                column: "ImageFilename",
+                value: "ecuador.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 29,
+                column: "ImageFilename",
+                value: "peru.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 30,
+                column: "ImageFilename",
+                value: "ethiopia.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 31,
+                column: "ImageFilename",
+                value: "kenya.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 32,
+                column: "ImageFilename",
+                value: "nigeria.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 33,
+                column: "ImageFilename",
+                value: "rwanda.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 34,
+                column: "ImageFilename",
+                value: "south-africa.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 35,
+                column: "ImageFilename",
+                value: "zimbabwe.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 36,
+                column: "ImageFilename",
+                value: "antarctica.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Trip",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Trip",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Tripstop",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Enddate", "Startdate" },
+                values: new object[] { new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Tripstop",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Enddate", "Startdate" },
+                values: new object[] { new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Tripstop",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Enddate", "Startdate" },
+                values: new object[] { new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Tripstop",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Enddate", "Startdate" },
+                values: new object[] { new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 28,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 29,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 30,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 31,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 32,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 33,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 34,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 35,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 36,
+                column: "ImageFilename",
+                value: "no-image.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Trip",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Trip",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Tripstop",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Enddate", "Startdate" },
+                values: new object[] { new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Tripstop",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Enddate", "Startdate" },
+                values: new object[] { new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Tripstop",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Enddate", "Startdate" },
+                values: new object[] { new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Tripstop",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Enddate", "Startdate" },
+                values: new object[] { new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+    }
+}
